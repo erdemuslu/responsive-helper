e0b6100679a633ebf0c9f610f8eb1ed2c02699dc
Erdem <erdem@erdems-mbp.mynet>
2019-03-12 11:06:03 +0300

    add helper function for createEl & appendEl

---
+++ responsive-helper.js
@@ -0,0 +1,65 @@
+// create class
+const className = `f-responsive-helper-${String(Math.floor(Math.random() * 1000))}`;
+
+// define props
+const indexArr = [];
+const elements = document.querySelectorAll('body *');
+
+// create zIndex arr
+elements.forEach(item => {
+  if (window.getComputedStyle(item).getPropertyValue('z-index') !== 'undefined') {
+    if (!isNaN(Number(window.getComputedStyle(item).getPropertyValue('z-index')))) {
+      indexArr.push(Number(window.getComputedStyle(item).getPropertyValue('z-index')));
+    }
+  }
+});
+
+// helper fuction: append div
+const appendElement = ({ el, target }) => {
+  // append
+  document.querySelector(target).appendChild(el);
+}
+
+// helper fuction: create div
+const createElement = (type) => {
+  // create div el
+  const el = document.createElement(type);
+  
+  // set style
+  el.setAttribute(
+    'style',
+    `font-family: monospace !important; position: fixed; z-index: ${indexArr.sort().pop() || 2}; top: 4px; right: 0px; background-color: lightgray; font-size: 12px; padding: 4px; color: black;`
+  );
+  
+  // set class
+  el.setAttribute(
+    'class',
+    className
+  );
+
+  return el;
+}
+
+// run create element helper
+const el = createElement('div');
+
+// catch resize action
+window.addEventListener('resize', () => {
+  // append el
+  appendElement({ el, target: 'html' });
+
+  // get window size
+  const { innerWidth, innerHeight } = window;
+
+  // render size
+  el.innerHTML = `
+    ${innerWidth}px
+    <span style="margin: 0 4px; font-size: 10px;">x</span>
+    ${innerHeight}px
+  `
+
+  // clear dom
+  setTimeout(() => {
+    document.querySelector(`.${className}`).remove();
+  }, 2000);
+});
